f207602148d47749c422c0422e494d774da2456b
Mathis Matyas-Istvan <mathismatyas-istvan@Mathiss-MacBook-Air.local>
2026-01-24 11:37:36 +0100

    Initialized project

---
+++ dailytodolist/dailytodolistApp.swift
@@ -0,0 +1,17 @@
+//
+//  dailytodolistApp.swift
+//  dailytodolist
+//
+//  Created by Mathis Matyas-Istvan on 24.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct dailytodolistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
